3e63f1a405a82e15b3dc535146e98c780e7bf7d8
guillemleon <guillem.leonf@gmail.com>
2025-04-17 11:27:50 +0200

    Added contact page

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import Layout from '../../components/layout';
+import './index.scss';
+import { useTranslation } from 'react-i18next';
 
 function Contact() {
-  return <Layout>Contact</Layout>;
+  const { t } = useTranslation();
+
+  return (
+    <Layout>
+      <div className="contact-container">
+        <h1 className="contact-title">{t('CONTACT_TITLE')}</h1>
+        <p className="contact-subtitle">{t('CONTACT_SUBTITLE')}</p>
+        <span className="contact-separator">--</span>
+        <p className="contact-email">
+          <span>{t('CONTACT_COMMAND_LINE')}</span> {t('CONTACT_EMAIL')}
+        </p>
+      </div>
+    </Layout>
+  );
 }
 
 export default Contact;

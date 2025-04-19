f891d348e517e2df994fa94b6b84782411109c57
guillemleon <guillem.leonf@gmail.com>
2025-04-19 15:21:09 +0200

    Added 404 page

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Layout from '../../components/layout';
+import { useTranslation } from 'react-i18next';
+import './index.scss';
 
 function NotFound() {
-  return <div>404 Page Not Found</div>;
+  const { t } = useTranslation();
+
+  return (
+    <Layout>
+      <div className="page-not-found-container">
+        <h1>404</h1>
+        <p>{t('PAGE_NOT_FOUND')}</p>
+      </div>
+    </Layout>
+  );
 }
 
 export default NotFound;

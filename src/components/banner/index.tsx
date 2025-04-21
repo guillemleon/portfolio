a419714ca7c346bf9b16a1f8c4ae56507fad76df
guillemleon <guillem.leonf@gmail.com>
2025-04-21 23:19:36 +0200

    Fixed broken light theme

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -5,8 +5,9 @@ import caixabankLogo from '../../assets/logos/caixabank.png';
 import keolisLogo from '../../assets/logos/keolis.png';
 import extiaLogo from '../../assets/logos/extia.png';
 import flykubeLogo from '../../assets/logos/flykube.png';
+import eatkubeLogo from '../../assets/logos/eatkube.png';
 
-const bannerImages = [filminLogo, caixabankLogo, keolisLogo, extiaLogo, flykubeLogo];
+const bannerImages = [filminLogo, caixabankLogo, keolisLogo, extiaLogo, flykubeLogo, eatkubeLogo];
 
 const repeatedBannerImages = Array(5).fill(bannerImages).flat();
 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,23 +3,27 @@ import { useTranslation } from 'react-i18next';
 import DOMPurify from 'dompurify';
 import './index.scss';
 import Layout from '../../components/layout';
-import icon from '../../assets/icons/icon_white.png';
-
-const GRID_SIZE = 20;
+import iconWhite from '../../assets/icons/icon_white.png';
+import iconBlack from '../../assets/icons/icon_black.png';
+import { useTheme } from '../../context/ThemeContext';
 
 function Home() {
   const { t } = useTranslation();
-  const cells = Array.from({ length: GRID_SIZE * GRID_SIZE });
+  const { theme } = useTheme();
 
   const safeHTMLSubtitle = useMemo(() => {
     return DOMPurify.sanitize(t('HOME_SUBTITLE'));
   }, [t]);
 
+  const logo = useMemo(() => {
+    return theme === 'dark' ? iconWhite : iconBlack;
+  }, [theme]);
+
   return (
     <Layout isBannerVisible={true}>
       <div className="home-container">
         <div className="home-title-container">
-          <img src={icon} />
+          <img src={logo} />
           <h1 className="home-title">{t('HOME_TITLE')}</h1>
         </div>
         <h2 className="home-subtitle" dangerouslySetInnerHTML={{ __html: safeHTMLSubtitle }}></h2>

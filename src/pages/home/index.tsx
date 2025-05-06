3f8e0933745b661bb8f6dece9861a89bcbfbc227
guillemleon <guillem.leonf@gmail.com>
2025-05-06 21:38:39 +0200

    Added new logo and updated home design

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,8 +3,8 @@ import { useTranslation } from 'react-i18next';
 import DOMPurify from 'dompurify';
 import './index.scss';
 import Layout from '../../components/layout';
-import iconWhite from '../../assets/icons/icon_white.png';
-import iconBlack from '../../assets/icons/icon_black.png';
+import iconWhite from '../../assets/img/logo-512-white.png';
+import iconBlack from '../../assets/img/logo-512-black.png';
 import { useTheme } from '../../context/ThemeContext';
 
 function Home() {
@@ -20,13 +20,14 @@ function Home() {
   }, [theme]);
 
   return (
-    <Layout isBannerVisible={true}>
+    <Layout>
       <div className="home-container">
-        <div className="home-title-container">
-          <img src={logo} />
-          <h1 className="home-title">{t('HOME_TITLE')}</h1>
-        </div>
-        <h2 className="home-subtitle" dangerouslySetInnerHTML={{ __html: safeHTMLSubtitle }}></h2>
+        {/* <h1 className="home-title">{t('HOME_TITLE')}</h1> */}
+        <img className="logo" src={logo} />
+        <div className="logo-spinner-inner" />
+        <div className="logo-spinner-outer" />
+        <div className="logo-spinner-outer-outer" />
+        {/* <h2 className="home-subtitle" dangerouslySetInnerHTML={{ __html: safeHTMLSubtitle }}></h2> */}
       </div>
     </Layout>
   );

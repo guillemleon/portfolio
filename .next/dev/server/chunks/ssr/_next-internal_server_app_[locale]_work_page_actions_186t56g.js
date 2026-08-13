ddd2d0176cedd7e4b976d1799fa46db611960e1d
Guillem Leon Font <guillem.leonf@gmail.com>
2026-08-13 22:10:40 +0200

    Remove build artifacts from version control

--- .next/dev/server/chunks/ssr/_next-internal_server_app_[locale]_work_page_actions_186t56g.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/[locale]/work/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_%5Blocale%5D_work_page_actions_186t56g.js.map
--- .next/dev/static/chunks/_1g16bqw._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/node_modules_0kbivoc._.js","static/chunks/_1mlwj6w._.js","static/chunks/src_05ufnq6._.css","static/chunks/src_app_[locale]_page_module_06koyhx.css"],
-    source: "entry"
-});
